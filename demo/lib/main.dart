00910c0a34fc121f4931bbcfceced052a0e2e58c
Kuba Wernerowski <kubawernerowski@gmail.com>
2019-06-23 07:36:12 -0400

    Added permissions for ios and added picking photos from gallery

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:async';
+import 'dart:io';
 
 void main() {
   runApp(MaterialApp(
@@ -12,9 +15,8 @@ void main() {
           case '/tests':
             return MaterialPageRoute(builder: (context) => Tests());
             break;
-          case '/malaria-image':
-            return MaterialPageRoute(
-                builder: (context) => MalariaUploadImage());
+          case '/malaria':
+            return MaterialPageRoute(builder: (context) => Malaria());
             break;
         }
       }));
@@ -22,12 +24,12 @@ void main() {
 
 class Home extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          alignment: Alignment.topCenter,
-          padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-          child: Stack(children: <Widget>[
+    return (Scaffold(
+      body: Container(
+        alignment: Alignment.topCenter,
+        padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+        child: Stack(
+          children: <Widget>[
             Container(
               alignment: Alignment.topCenter,
               child: Image.asset(
@@ -55,22 +57,22 @@ class Home extends StatelessWidget {
                 ),
               ),
             ),
-          ]),
+          ],
         ),
       ),
-    );
+    ));
   }
 }
 
 class Tests extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          alignment: Alignment.topCenter,
-          padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-          child: Stack(children: <Widget>[
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.topCenter,
+        padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+        child: Stack(
+          children: <Widget>[
             Container(
               alignment: Alignment.topCenter,
               child: Image.asset(
@@ -78,56 +80,72 @@ class Tests extends StatelessWidget {
               ),
             ),
             Container(
-                alignment: Alignment.bottomCenter,
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
-                child: Row(
-                  children: <Widget>[
-                    RaisedButton(
-                      onPressed: () {
-                        // Navigate back to Home
-                        Navigator.pop(context);
-                      },
-                      textColor: Colors.white,
-                      color: Colors.blue[700],
-                      shape: StadiumBorder(),
-                      padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
-                      child: new Text(
-                        "Home",
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          letterSpacing: 3,
-                          fontWeight: FontWeight.w400,
-                        ),
+              alignment: Alignment.bottomCenter,
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
+              child: Row(
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () {
+                      // Navigate back to Home
+                      Navigator.pop(context);
+                    },
+                    textColor: Colors.white,
+                    color: Colors.blue[700],
+                    shape: StadiumBorder(),
+                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                    child: new Text(
+                      "Home",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 3,
+                        fontWeight: FontWeight.w400,
                       ),
                     ),
-                    RaisedButton(
-                      onPressed: () {
-                        // Navigate to Malaria Upload Image
-                        Navigator.pushNamed(context, '/malaria-image');
-                      },
-                      textColor: Colors.white,
-                      color: Colors.blue[700],
-                      shape: StadiumBorder(),
-                      padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
-                      child: new Text(
-                        "Malaria",
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          letterSpacing: 3,
-                          fontWeight: FontWeight.w400,
-                        ),
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      // Navigate to Malaria Upload Image
+                      Navigator.pushNamed(context, '/malaria');
+                    },
+                    textColor: Colors.white,
+                    color: Colors.blue[700],
+                    shape: StadiumBorder(),
+                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                    child: new Text(
+                      "Malaria",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 3,
+                        fontWeight: FontWeight.w400,
                       ),
                     ),
-                  ],
-                )),
-          ]),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class MalariaUploadImage extends StatelessWidget {
+class Malaria extends StatefulWidget {
+  @override
+  _MalariaState createState() => _MalariaState();
+}
+
+class _MalariaState extends State<Malaria> {
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -135,18 +153,22 @@ class MalariaUploadImage extends StatelessWidget {
         title: Text('Malaria Tests'),
       ),
       body: Center(
-          child: Row(
-        children: <Widget>[
-          RaisedButton(
-            onPressed: () {
-              // Navigate back to HomeScreen
-              Navigator.pop(context);
-            },
-            child: Text('Back to Tests'),
-          ),
-        ],
-      )),
+        child: _image == null ? Text('Select an Image') : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
     );
+    // raisedButton: RaisedButton(
+    //       onPressed: () {
+    //         // Navigate back to HomeScreen
+    //         Navigator.pop(context);
+    //       },
+    //       child: Text('Back to Tests'),
+    //     ),
+    // );
   }
 }
 
