f15570a85feb04d8869a30c6a04360e404ff0ee0
Kuba Wernerowski <kubawernerowski@gmail.com>
2019-06-23 12:46:26 -0400

    Removed unneeded JS code. Final touch ups.

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -20,6 +20,9 @@ void main() {
         case '/malaria':
           return MaterialPageRoute(builder: (context) => Malaria());
           break;
+        case '/parkinsons':
+          return MaterialPageRoute(builder: (context) => Parkinsons());
+          break;
       }
     },
     theme: ThemeData(fontFamily: 'Roboto'),
@@ -81,132 +84,124 @@ class Tests extends StatelessWidget {
           ),
         ),
         child: Align(
-            alignment: Alignment.center,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(bottom: 40),
-                  child: Image.asset(
-                    'images/aceso-logo-single.png',
-                    width: 350,
-                  ),
+          alignment: Alignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom: 40),
+                child: Image.asset(
+                  'images/aceso-logo-single.png',
+                  width: 350,
                 ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 25.0),
-                  child: Text(
-                    'SELECT YOUR TEST',
-                    style: TextStyle(fontSize: 25, letterSpacing: 5),
-                  ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 25.0),
+                child: Text(
+                  'SELECT YOUR TEST',
+                  style: TextStyle(fontSize: 25, letterSpacing: 5),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 20.0),
-                  child: RaisedButton(
-                    onPressed: () {
-                      // Navigate to Malaria Upload Image
-                      Navigator.pushNamed(context, '/malaria');
-                    },
-                    textColor: Colors.white,
-                    color: Colors.blue[700],
-                    shape: StadiumBorder(),
-                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
-                    child: new Text(
-                      "MALARIA",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        letterSpacing: 3,
-                        fontWeight: FontWeight.w400,
-                      ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    // Navigate to Malaria Upload Image
+                    Navigator.pushNamed(context, '/malaria');
+                  },
+                  textColor: Colors.white,
+                  color: Colors.blue[700],
+                  shape: StadiumBorder(),
+                  padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                  child: new Text(
+                    "MALARIA",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      letterSpacing: 3,
+                      fontWeight: FontWeight.w400,
                     ),
                   ),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 20.0),
-                  child: RaisedButton(
-                    onPressed: () {
-                      // Navigate to Malaria Upload Image
-                      Navigator.pushNamed(context, '/malaria');
-                    },
-                    textColor: Colors.white,
-                    color: Colors.blue[700],
-                    shape: StadiumBorder(),
-                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
-                    child: new Text(
-                      "PARKINSONS",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        letterSpacing: 3,
-                        fontWeight: FontWeight.w400,
-                      ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    // Navigate to Malaria Upload Image
+                    Navigator.pushNamed(context, '/parkinsons');
+                  },
+                  textColor: Colors.white,
+                  color: Colors.blue[700],
+                  shape: StadiumBorder(),
+                  padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                  child: new Text(
+                    "PARKINSONS",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      letterSpacing: 3,
+                      fontWeight: FontWeight.w400,
                     ),
                   ),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 20.0),
-                  child: RaisedButton(
-                    onPressed: () {
-                      // Navigate to Malaria Upload Image
-                      Navigator.pushNamed(context, '/malaria');
-                    },
-                    textColor: Colors.white,
-                    color: Colors.blue[700],
-                    shape: StadiumBorder(),
-                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
-                    child: new Text(
-                      "SKIN CANCER",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        letterSpacing: 3,
-                        fontWeight: FontWeight.w400,
-                      ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                child: RaisedButton(
+                  onPressed: null,
+                  textColor: Colors.white,
+                  color: Colors.blue[700],
+                  shape: StadiumBorder(),
+                  padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                  child: new Text(
+                    "SKIN CANCER",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      letterSpacing: 3,
+                      fontWeight: FontWeight.w400,
                     ),
                   ),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 20.0),
-                  child: RaisedButton(
-                    onPressed: () {
-                      // Navigate to Malaria Upload Image
-                      Navigator.pushNamed(context, '/malaria');
-                    },
-                    textColor: Colors.white,
-                    color: Colors.blue[700],
-                    shape: StadiumBorder(),
-                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
-                    child: new Text(
-                      "HUNTINGTONS",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        letterSpacing: 3,
-                        fontWeight: FontWeight.w400,
-                      ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                child: RaisedButton(
+                  onPressed: null,
+                  textColor: Colors.white,
+                  color: Colors.blue[700],
+                  shape: StadiumBorder(),
+                  padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                  child: new Text(
+                    "HUNTINGTONS",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      letterSpacing: 3,
+                      fontWeight: FontWeight.w400,
                     ),
                   ),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 20.0, bottom: 40.0),
-                  child: RaisedButton(
-                    onPressed: () {
-                      // Navigate to Malaria Upload Image
-                      Navigator.pushNamed(context, '/malaria');
-                    },
-                    textColor: Colors.white,
-                    color: Colors.blue[700],
-                    shape: StadiumBorder(),
-                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
-                    child: new Text(
-                      "ALZHEIMERS",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        letterSpacing: 3,
-                        fontWeight: FontWeight.w400,
-                      ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20.0, bottom: 40.0),
+                child: RaisedButton(
+                  onPressed: null,
+                  textColor: Colors.white,
+                  color: Colors.blue[700],
+                  shape: StadiumBorder(),
+                  padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                  child: new Text(
+                    "ALZHEIMERS",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      letterSpacing: 3,
+                      fontWeight: FontWeight.w400,
                     ),
                   ),
                 ),
-              ],
-            )),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -218,28 +213,34 @@ class Malaria extends StatefulWidget {
 }
 
 class _MalariaState extends State<Malaria> {
-  final String flaskEndPoint = 'http://52.233.29.248:80/predict';
+  final String malariaEndpoint = 'http://52.233.29.248:80/predict';
   File _image;
+  String _result;
 
   Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.gallery);
 
     setState(() {
       _image = image;
-      _uploadIamge();
+      _uploadImage();
     });
   }
 
-  void _uploadIamge() async {
+  void _uploadImage() async {
     if (_image == null) return;
     String base64Image = base64Encode(_image.readAsBytesSync());
     String fileName = _image.path.split('/').last;
 
-    http.post(flaskEndPoint, body: {
+    http.post(malariaEndpoint, body: {
       "image": base64Image,
       "name": fileName,
     }).then((res) {
-      print(res.statusCode);
+      var data = json.decode(res.body);
+      if (data.toString().contains('Clean')) {
+        _result = "Clean";
+      } else {
+        _result = "Malaria Detected";
+      }
     }).catchError((err) {
       print(err);
     });
@@ -280,12 +281,114 @@ class _MalariaState extends State<Malaria> {
               ),
             ),
             Container(
-              margin: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-              width: 350,
-              height: 200,
-              child:
-                  _image == null ? Text('Select an Image') : Image.file(_image),
+                margin: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                width: 350,
+                height: 200,
+                child: Column(
+                  children: <Widget>[
+                    _image == null
+                        ? Text('Select an Image')
+                        : Image.file(_image),
+                    _result == null ? Text("") : Text(_result),
+                  ],
+                )),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+}
+
+class Parkinsons extends StatefulWidget {
+  @override
+  _ParkinsonsState createState() => _ParkinsonsState();
+}
+
+class _ParkinsonsState extends State<Parkinsons> {
+  final String parkinsonsEndpoint = 'http://52.233.29.248:80/predictp';
+  File _image;
+  String _result;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = image;
+      _uploadImage();
+    });
+  }
+
+  void _uploadImage() async {
+    if (_image == null) return;
+    String base64Image = base64Encode(_image.readAsBytesSync());
+    String fileName = _image.path.split('/').last;
+
+    http.post(parkinsonsEndpoint, body: {
+      "image": base64Image,
+      "name": fileName,
+    }).then((res) {
+      var data = json.decode(res.body);
+      if (data.toString().contains('Healthy')) {
+        _result = "Healthy";
+      } else {
+        _result = "Parkinsons Detected";
+      }
+    }).catchError((err) {
+      print(err);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Parkinsons Tests'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 30, right: 30),
+              child: Text(
+                '\nPARKINSONS TESTING',
+                style: TextStyle(
+                  fontSize: 25,
+                  letterSpacing: 4,
+                  color: Colors.blue[900],
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
             ),
+            // Container(
+            //   alignment: Alignment.centerLeft,
+            //   padding: EdgeInsets.only(left: 30, right: 30),
+            //   child: Text(
+            //     '\nSYMPTOMS MAY INCLUDE:\n\n • SHAKING CHILLS\n • HIGH FEVER\n • PROFUSE SWEATING\n\nUPLOAD AN IMAGE OF A CELL:',
+            //     style: TextStyle(
+            //       fontSize: 16,
+            //       height: 1.2,
+            //       letterSpacing: 2,
+            //     ),
+            //   ),
+            // ),
+            Container(
+                margin: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                width: 350,
+                height: 350,
+                child: Column(
+                  children: <Widget>[
+                    _image == null
+                        ? Text('Select an Image')
+                        : Image.file(_image),
+                    _result == null ? Text("") : Text(_result),
+                  ],
+                )),
           ],
         ),
       ),
