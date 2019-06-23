0d167f76248cd43c86fd6a3ebd5f62166e88f927
GauravRanganath <46269824+GauravRanganath@users.noreply.github.com>
2019-06-23 06:41:46 -0400

    temp

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -64,20 +64,108 @@ class SecondRoute extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          alignment: Alignment.topCenter,
           padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-          child: Stack(
+          child: Column(
               children: <Widget>[
+
                 Container(
-                  alignment: Alignment.topCenter,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
                   child:
-                  Image.asset(
-                    'images/aceso-logo-2.png',
+                  new RaisedButton(
+                    onPressed: () {
+
+                    },
+                    textColor: Colors.white,
+                    color: Colors.blue[700],
+                    shape: StadiumBorder(),
+                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                    child: new Text(
+                      "START",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 3,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
                   ),
                 ),
 
                 Container(
-                  alignment: Alignment.bottomCenter,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
+                  child:
+                  new RaisedButton(
+                    onPressed: () {
+
+                    },
+                    textColor: Colors.white,
+                    color: Colors.blue[700],
+                    shape: StadiumBorder(),
+                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                    child: new Text(
+                      "START",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 3,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
+                  child:
+                  new RaisedButton(
+                    onPressed: () {
+
+                    },
+                    textColor: Colors.white,
+                    color: Colors.blue[700],
+                    shape: StadiumBorder(),
+                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                    child: new Text(
+                      "START",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 3,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
+                  child:
+                  new RaisedButton(
+                    onPressed: () {
+
+                    },
+                    textColor: Colors.white,
+                    color: Colors.blue[700],
+                    shape: StadiumBorder(),
+                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                    child: new Text(
+                      "START",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 3,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
                   padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
                   child:
                   new RaisedButton(
@@ -101,6 +189,8 @@ class SecondRoute extends StatelessWidget {
               ]
           ),
         ),
+
+
       ),
     );
   }
