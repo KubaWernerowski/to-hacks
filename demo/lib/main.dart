653b79ff0f3dd9fc2dd84621e57ab176cf5a2838
Kuba Wernerowski <kubawernerowski@gmail.com>
2019-06-23 06:40:21 -0400

    Updated navigation to allow multiple screens

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -2,63 +2,67 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Navigation Basics',
-    home: FirstRoute(),
-  ));
+      title: 'Navigation Basics',
+      home: Home(),
+      onGenerateRoute: (RouteSettings settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(builder: (context) => Home());
+            break;
+          case '/tests':
+            return MaterialPageRoute(builder: (context) => Tests());
+            break;
+          case '/malaria-image':
+            return MaterialPageRoute(
+                builder: (context) => MalariaUploadImage());
+            break;
+        }
+      }));
 }
 
-class FirstRoute extends StatelessWidget {
+class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
           alignment: Alignment.topCenter,
           padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-          child: Stack(
-              children: <Widget>[
-                Container(
-                  alignment: Alignment.topCenter,
-                  child:
-                  Image.asset(
-                    'images/aceso-logo.png',
+          child: Stack(children: <Widget>[
+            Container(
+              alignment: Alignment.topCenter,
+              child: Image.asset(
+                'images/aceso-logo.png',
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
+              child: new RaisedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/tests');
+                },
+                textColor: Colors.white,
+                color: Colors.blue[700],
+                shape: StadiumBorder(),
+                padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                child: new Text(
+                  "START DIAGNOSIS",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 3,
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
-
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
-                  child:
-                  new RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => SecondRoute()),
-                      );
-                    },
-                    textColor: Colors.white,
-                    color: Colors.blue[700],
-                    shape: StadiumBorder(),
-                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
-                    child: new Text(
-                      "START DIAGNOSIS",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        letterSpacing: 3,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
-                ),
-              ]
-          ),
+              ),
+            ),
+          ]),
         ),
       ),
     );
   }
-
 }
 
-class SecondRoute extends StatelessWidget {
+class Tests extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -66,43 +70,105 @@ class SecondRoute extends StatelessWidget {
         body: Container(
           alignment: Alignment.topCenter,
           padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-          child: Stack(
-              children: <Widget>[
-                Container(
-                  alignment: Alignment.topCenter,
-                  child:
-                  Image.asset(
-                    'images/aceso-logo-2.png',
-                  ),
-                ),
-
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
-                  child:
-                  new RaisedButton(
-                    onPressed: () {
-
-                    },
-                    textColor: Colors.white,
-                    color: Colors.blue[700],
-                    shape: StadiumBorder(),
-                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
-                    child: new Text(
-                      "START",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        letterSpacing: 3,
-                        fontWeight: FontWeight.w400,
+          child: Stack(children: <Widget>[
+            Container(
+              alignment: Alignment.topCenter,
+              child: Image.asset(
+                'images/aceso-logo-2.png',
+              ),
+            ),
+            Container(
+                alignment: Alignment.bottomCenter,
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
+                child: Row(
+                  children: <Widget>[
+                    RaisedButton(
+                      onPressed: () {
+                        // Navigate back to Home
+                        Navigator.pop(context);
+                      },
+                      textColor: Colors.white,
+                      color: Colors.blue[700],
+                      shape: StadiumBorder(),
+                      padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                      child: new Text(
+                        "Home",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          letterSpacing: 3,
+                          fontWeight: FontWeight.w400,
+                        ),
                       ),
                     ),
-                  ),
-                ),
-              ]
-          ),
+                    RaisedButton(
+                      onPressed: () {
+                        // Navigate to Malaria Upload Image
+                        Navigator.pushNamed(context, '/malaria-image');
+                      },
+                      textColor: Colors.white,
+                      color: Colors.blue[700],
+                      shape: StadiumBorder(),
+                      padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                      child: new Text(
+                        "Malaria",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          letterSpacing: 3,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+          ]),
         ),
       ),
     );
   }
 }
 
+class MalariaUploadImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Malaria Tests'),
+      ),
+      body: Center(
+          child: Row(
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () {
+              // Navigate back to HomeScreen
+              Navigator.pop(context);
+            },
+            child: Text('Back to Tests'),
+          ),
+        ],
+      )),
+    );
+  }
+}
+
+class SlideRightRoute extends PageRouteBuilder {
+  final Widget widget;
+  SlideRightRoute({this.widget})
+      : super(
+          pageBuilder: (BuildContext context, Animation<double> animation,
+              Animation<double> secondaryAnimation) {
+            return widget;
+          },
+          transitionsBuilder: (BuildContext context,
+              Animation<double> animation,
+              Animation<double> secondaryAnimation,
+              Widget child) {
+            return new SlideTransition(
+              position: new Tween<Offset>(
+                begin: const Offset(1.0, 0.0),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
+}
